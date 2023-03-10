cf43eff3c6f2f8568aee779cbdc66be23cdf6a2f
GNReplay <52163755+GNReplay@users.noreply.github.com>
2023-03-10 15:23:34 +0700

    Initial commit

---
+++ app/src/main/java/hcmute/edu/vn/lab2_1_week5/MainActivity.java
@@ -0,0 +1,52 @@
+package hcmute.edu.vn.lab2_1_week5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtDataIntend;
+    private Button btnStartService;
+    private Button btnStopService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtDataIntend = findViewById(R.id.edit_data_intent);
+        btnStartService = findViewById(R.id.btn_start_service);
+        btnStopService = findViewById(R.id.btn_stop_service);
+
+        btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickStartService();
+            }
+        });
+
+        btnStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickStopService();
+            }
+        });
+    }
+
+    private void clickStopService() {
+        Intent intent =  new Intent(this, MyService.class);
+        stopService(intent);
+
+    }
+
+    private void clickStartService() {
+        Intent intent =  new Intent(this, MyService.class);
+        intent.putExtra("key_data_intent", edtDataIntend.getText().toString().trim());
+        startService(intent);
+    }
+}
